7868574b4ab57bf691a304fdef40263c2574b61f
William Wong <compulim@hotmail.com>
2018-12-16 22:54:09 -0800

    Reset

--- src/index.js
+++
@@ -1,3 +0,0 @@
-import bookshelf from './bookshelf'
-
-export default bookshelf
--- src/removeKey.js
+++
@@ -1,9 +0,0 @@
-export default function removeKey(map, ...keys) {
-  return Object.keys(map).reduce((nextMap, key) => {
-    if (!keys.includes(key)) {
-      nextMap[key] = map[key];
-    }
-
-    return nextMap;
-  }, {});
-}
